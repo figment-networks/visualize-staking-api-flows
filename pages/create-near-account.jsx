963c00cd718fb62cc0a45944990f0ffd1220cd5e
Rogan X <rogan@figment.io>
2022-12-08 15:02:07 -0700

    Fix missing import

--- pages/create-near-account.jsx
+++ pages/create-near-account.jsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import {useState} from "react";
+import React, {useState} from "react";
 import styles from "../styles/Home.module.css";
 import Image from "next/image";
 
